aedaba55cb50bd6c5e63fb069e3751d88fc72427
Jefferson Moraes <jefferson-moraes@outlook.com>
2023-05-05 11:17:56 -0300

    feat: adding currency api

--- src/currencyGatewayHttp.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { CurrencyGateway } from './currencyGateway'
-
-export class CurrencyGatewayHttp implements CurrencyGateway {
-  async getCurrencies(): Promise<any> {
-    const response = await axios.get('http://localhost:3001/currencies')
-    return response.data
-  }
-}
